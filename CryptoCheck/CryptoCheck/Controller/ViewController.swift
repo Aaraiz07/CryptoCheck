826e8c622fca6d69220cc6fa13bf10ae133346ac
M. Aaraiz Wasim <68289140+Aaraiz07@users.noreply.github.com>
2023-10-24 22:34:33 +0500

    Add networking

---
+++ CryptoCheck/CryptoCheck/Controller/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  CryptoCheck
+//
+//  Created by Aaraiz Wasim on 22/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, CryptoManagerDelegate {
+    
+    var cryptoManager = CryptoManager()
+
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
+        cryptoManager.delegate = self
+    }
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return cryptoManager.currencyArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return cryptoManager.currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedCurrency = cryptoManager.currencyArray[row]
+        cryptoManager.getCoinPrice(for: selectedCurrency)
+    }
+    
+    func didUpdatePrice(price: String, currency: String) {
+            
+            //Remember that we need to get hold of the main thread to update the UI, otherwise our app will crash if we
+            //try to do this from a background thread (URLSession works in the background).
+            DispatchQueue.main.async {
+                self.bitcoinLabel.text = price
+                self.currencyLabel.text = currency
+            }
+        }
+        
+        func didFailWithError(error: Error) {
+            print(error)
+        }
+
+}
+
---
+++ CryptoCheck/CryptoCheck/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  CryptoCheck
+//
+//  Created by Aaraiz Wasim on 24/10/2023.
+//
+
+import Foundation
+
+struct CoinData: Codable {
+    
+    let rate: Double
+}
